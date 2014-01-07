4c842a2ff01cd5ddd16fda13e431837f137c8c01
driKton <guaje-95@hotmail.com>
2014-01-07 16:55:06 +0000

    EMEGDT

---
+++ EMEGDT/modAPI.js
@@ -0,0 +1,26 @@
+﻿(function () {
+	var ready = function () {
+		//Añadimos Topics
+			EMEGDT.addTopicCityPolitics();
+		//Añadimos Platforms
+
+		//Añadimos eventos
+
+		//Añadimos researches
+			EMEGDT.addResearchFPS();
+			EMEGDT.addResearchTPS();
+			EMEGDT.addResearchSandBox();
+	};
+
+	var error = function () {
+	};
+
+	GDT.loadJs(['mods/EMEGDT/helpers/checks.js',
+	'mods/EMEGDT/api/persistence.js',
+	'mods/EMEGDT/api/events.js',
+	'mods/EMEGDT/api/platforms.js',
+	'mods/EMEGDT/api/topics.js',
+	'mods/EMEGDT/api/research.js',
+	'mods/EMEGDT/src/emegdt.js'
+	], ready, error);
+})();
---
+++ EMEGDT/src/emegdt.js
@@ -0,0 +1,64 @@
+﻿var EMEGDT = {};
+(function () {
+	//TOPICS
+	EMEGDT.addTopicCityPolitics = function () {
+		GDT.addTopics([
+		{
+			id: "CityPolitics",
+			name: "City Politics".localize("game topic"),
+			genreWeightings: [.4, .4, .7, 1, 1, .6],
+			audienceWeightings: [0.8, 1, 0.8]
+		}
+		]);
+	};
+
+	//PLATFORMS
+
+
+	//EVENTS
+
+
+	//RESEARCHES
+	EMEGDT.addResearchFPS = function () {
+		GDT.addResearchItem(
+		{
+			id: "FPS", //Must be unique
+			name: "First Person Shooter".localize(), //Display name
+			v: 6, //Tech level
+			canResearch: function (company) {
+				return LevelCalculator.getMissionLevel('Engine') > 6 //The "Level Design" level has to be at least 6
+			},
+			category: "Engine",
+			categoryDisplayName: "Engine".localize()
+		});
+	};
+	
+	EMEGDT.addResearchTPS = function () {
+		GDT.addResearchItem(
+		{
+			id: "TPS", //Must be unique
+			name: "Third Person Shooter".localize(), //Display name
+			v: 6, //Tech level
+			canResearch: function (company) {
+				return LevelCalculator.getMissionLevel('Engine') > 6 //The "Level Design" level has to be at least 6
+			},
+			category: "Engine",
+			categoryDisplayName: "Engine".localize()
+		});
+	};
+	
+	EMEGDT.addResearchSandBox = function () {
+		GDT.addResearchItem(
+		{
+			id: "sandbox", //Must be unique
+			name: "SandBox".localize(), //Display name
+			v: 6, //Tech level
+			canResearch: function (company) {
+				return LevelCalculator.getMissionLevel('Engine') > 6 //The "Level Design" level has to be at least 6
+			},
+			category: "Engine",
+			categoryDisplayName: "Engine".localize()
+		});
+	};
+	
+})();
